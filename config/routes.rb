b9c75bde095d6c7b804c5c24ea6c0f4a66f296f0
Lorenzo Masselli <lorenzomasselli1@gmail.com>
2023-05-25 12:37:34 +0100

    Validations for watchlist completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get 'lists/name'
+  get 'movies/new'
+  get 'movies/create'
+  get 'movies/index'
+  get 'movies/update'
+  get 'movies/destroy'
+  get 'movies/show'
+  get 'movies/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :movies
+
+  resources :lists
 end
